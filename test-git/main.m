cfb41e3f864853f533b4a162a949a8fc8433ee59
memosize <89068024@qq.com>
2017-04-01 00:36:19 +0800

    Initial Commit

---
+++ test-git/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test-git
+//
+//  Created by Yangsl on 2017/4/1.
+//  Copyright © 2017年 Yangsl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
